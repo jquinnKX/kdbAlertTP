a7b3a4377eb23b63e71b52340024aca97294c459
jquinnKX <46521586+jquinnKX@users.noreply.github.com>
2019-09-13 13:21:30 +0100

    Add files via upload

--- runMany.sh
+++ runMany.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 #q
-#echo Test 0a
-#q startUp.q -createMemberIDs 0b -replayFinishTime 00:05:00 -removeLogFile 1b -experimentName 5minsThread1
-#echo Test 1aretry2
-#q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -removeLogFile 0b -experimentName test1run1retry2
-#echo Test 2aretry2
-#q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -replayLogFileOnly 1b -removeLogFile 1b -experimentName test2run1retry2
-#echo Test 3retry2
-#q startUp.q -createMemberIDs 1b -replayFinishTime 04:00:00 -replayLogFileOnly 0b -removeLogFile 1b -excludeEachLoopTest 1b -experimentName test3run1retry2
-#echo Test 3b
-#q startUp.q -createMemberIDs 1b -replayFinishTime 04:00:00 -replayLogFileOnly 0b -removeLogFile 1b -excludeEachLoopTest 1b -experimentName test3run2
-#echo Test 3c
-#q startUp.q -createMemberIDs 1b -replayFinishTime 04:00:00 -replayLogFileOnly 0b -removeLogFile 1b -excludeEachLoopTest 1b -experimentName test3run3
-#echo Test 4retry2
-#q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 0 -removeLogFile 1b -experimentName test4run1retry2
+#echo healthcheck
+#../q/l64/q startUp.q -createMemberIDs 0b -replayFinishTime 00:00:20 -removeLogFile 1b -experimentName healthcheck
+#
+#
+echo Test1Run1
+../q/l64/q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -removeLogFile 0b -experimentName Test1Run1
+echo Test2Run1
+../q/l64/q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -replayLogFileOnly 1b -removeLogFile 1b -experimentName Test2Run1
+echo Test1Run2
+../q/l64/q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -removeLogFile 0b -experimentName Test1Run2
+echo Test2Run2
+../q/l64/q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -replayLogFileOnly 1b -removeLogFile 1b -experimentName Test2Run2
+#echo Test1Run3
+#../q/l64/q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -removeLogFile 0b -experimentName Test1Run3
+#echo Test2Run3
+#../q/l64/q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -replayLogFileOnly 1b -removeLogFile 1b -experimentName Test2Run3
+#
+#
+#echo Test3Run1
+#../q/l64/q startUp.q -createMemberIDs 1b -replayFinishTime 04:00:00 -replayLogFileOnly 0b -removeLogFile 1b -excludeEachLoopTest 1b -experimentName Test3Run1
+#echo Test3Run2
+#../q/l64/q startUp.q -createMemberIDs 1b -replayFinishTime 04:00:00 -replayLogFileOnly 0b -removeLogFile 1b -excludeEachLoopTest 1b -experimentName Test3Run2
+#echo Test3Run3
+#../q/l64/q startUp.q -createMemberIDs 1b -replayFinishTime 04:00:00 -replayLogFileOnly 0b -removeLogFile 1b -excludeEachLoopTest 1b -experimentName Test3Run3
+#
+#
+#echo Test 4
+#../q/l64/q startUp.q -createMemberIDs 1b -replayFinishTime 12:00:00 -replayLogFileOnly 0b -removeLogFile 1b -excludeEachLoopTest 1b -experimentName Test4
+#
+#
+#
+#IGNORE
+#
+#
 #echo Test 4b
-#q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 0 -removeLogFile 1b -experimentName test4run2
-#echo Test 5aretry
-#q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 4 -removeLogFile 1b -experimentName test5run1retry
+#../q/l64/q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 0 -removeLogFile 1b -experimentName test4run2
+#echo Test 5aetry
+#../q/l64/q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 4 -removeLogFile 1b -experimentName test5run1retry
 #echo Test 5b
-#q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 4 -removeLogFile 1b -experimentName test5run2
-#/echo Test 5retry2
-#q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 4 -removeLogFile 1b -experimentName test5run1retry2
-#echo Test 5With0Slaves
-#q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 0 -removeLogFile 1b -experimentName 5With0Slaves
-#echo Test 5With4Slaves
-#q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 4 -removeLogFile 1b -experimentName 5With4Slaves
-echo Test 5With4Slaves2
-q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 4 -removeLogFile 1b -experimentName 5With4Slaves2
-echo Test 5With4Slaves3
-q startUp.q -createMemberIDs 0b -replayFinishTime 01:00:00 -numberOfSlaves 4 -removeLogFile 1b -experimentName 5With4Slaves3
-#echo Test 6
-#q startUp.q -createMemberIDs 1b -replayFinishTime 12:00:00 -replayLogFileOnly 0b -removeLogFile 1b -excludeEachLoopTest 1b -experimentName test6run1
